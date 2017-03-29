57ecb6fde906e20d4bf4360ef02f087c42eaca1c
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-29 23:53:21 +0200

    webpack 2 example

---
+++ typescript-playground/webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack'); //to access built-in plugins
+module.exports = {
+    entry: './app.ts',
+    output: { filename: 'bundle.js' },
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [{ loader: "ts-loader" }]
+            }
+        ]
+    },
+    devtool: "source-map",
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ beautify: true, sourceMap: true })
+    ]
+}
