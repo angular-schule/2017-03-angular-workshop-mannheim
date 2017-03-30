e2516c43092bd03063070034874e9afa3c4af42a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-30 14:51:28 +0200

    displaying a list of strings

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  books: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.books = ['Angular', 'Das andere Buch', 'AngularJS'];
   }
 
 }
+
