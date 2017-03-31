c9cb19fbf78ef37e5845aaf614fe1ba08c463925
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-31 15:12:11 +0200

    cleanup

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChildren, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookComponent } from './../book/book.component';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
@@ -10,13 +10,8 @@ import { Book } from './../shared/book';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit,
-  AfterViewInit, OnDestroy {
+export class DashboardComponent implements OnInit {
   books: Book[];
-
-  @ViewChildren(BookComponent) bookComponents;
-
-  constructor() { }
 
   add(isbn: HTMLFormElement,
       title: HTMLFormElement,
@@ -27,31 +22,12 @@ export class DashboardComponent implements OnInit,
   }
 
   ngOnInit() {
-
-    // hier immer neu sortieren!
     this.books = [
       new Book('000', 'Angular', 'Zurück in die Zukunft', 2),
       new Book('111', 'AngularJS 1.x', 'Oldie but Goldie', 3),
       new Book('222', 'DAs andere Buch', 'Blah')
     ];
     this.reorderBooks();
-  }
-
-  ngAfterViewInit() {
-    this.bookComponents.forEach((bookComponent: BookComponent) => {
-      bookComponent.rated
-        .filter(b => b.title === 'Angular')
-        .map(b => b.isbn)
-        .subscribe((book) => {
-          console.log(book);
-        });
-    });
-  }
-
-  ngOnDestroy() {
-    this.bookComponents.forEach((bookComponent: BookComponent) => {
-      bookComponent.rated.unsubscribe();
-    });
   }
 
   reorderBooks() {
